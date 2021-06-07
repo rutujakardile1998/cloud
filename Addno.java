f354a8da7530bc45069b1a89770dba0a8dc6e38e
rutujakardile1998 <59498764+rutujakardile1998@users.noreply.github.com>
2021-06-07 15:33:46 +0530

    uploaded

---
+++ Addno.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Addno {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		
+		a=10;
+		b=30;
+		c=a+b;
+		System.out.println("Addition of no is:"+c);
+
+	}
+
+}
